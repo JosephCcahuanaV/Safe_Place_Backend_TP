e634fc08e7d50a44894849f87ab23cab9f900e32
Stephan-5 <angiesr5@hotmail.com>
2023-09-28 02:30:37 -0500

    Local and Payment controller

--- src/main/java/Backendproject/controllers/PaymentController.java
+++ src/main/java/Backendproject/controllers/PaymentController.java
@@ -14,6 +14,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class PaymentController {
-
+    @Autowired
+    PaymentService paymentService;
+    @GetMapping("/payments")
+    public ResponseEntity<List<Payment>> getAllReviews() {
+        List<Payment> payments = paymentService.listAll();
+        return new ResponseEntity<List<Payment>>(payments, HttpStatus.OK);
+    }
+    @PostMapping("/payments/create")
+    public ResponseEntity<Payment> createRenter(@RequestBody Payment payment) {
+        Payment newPayment = paymentService.save(payment);
+        return new ResponseEntity<Payment>(newPayment, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/payments/{id}")
+    public ResponseEntity<HttpStatus> deleteReview(@PathVariable("id") Long id) {
+        paymentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
 
