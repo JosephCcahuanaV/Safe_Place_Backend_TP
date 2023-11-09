7b1fcc669ea720771592d24b56c36021aac3b156
JosephCcahuanaV <u201717928@upc.edu.pe>
2023-11-09 16:39:43 -0500

    Actualizado Security2

--- src/main/java/Backendproject/controllers/UserSecurityController.java
+++ src/main/java/Backendproject/controllers/UserSecurityController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import Backendproject.security.SecurityUser;
 
 
-//RUTA
+//RUTAas
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
