0698eeca1f083bd88e559c942468629eca2d8821
JosephCcahuanaV <u201717928@upc.edu.pe>
2023-11-09 14:56:49 -0500

    Modificado

--- src/main/java/Backendproject/services/UsersecurityService.java
+++ src/main/java/Backendproject/services/UsersecurityService.java
@@ -1,10 +1,16 @@
 package Backendproject.services;
 
 
+import Backendproject.dtos.UserSecurityDTO;
 import Backendproject.entities.UserSecurity;
 
 public interface UsersecurityService {
 
-    public UserSecurity save(UserSecurity user);
+    public UserSecurity findById(Long id);
+
+    public UserSecurity register(UserSecurityDTO user);
+
+    public UserSecurity changePassword(UserSecurityDTO user);
+
 
 }
